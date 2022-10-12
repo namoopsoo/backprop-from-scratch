f1c5f29a62832b8c6b9c5118e33c9bb7aaa8bc1f
michal <namoopsoo>
2022-10-12 16:42:25 -0400

    more debug

--- test_feed_forward.py
+++ test_feed_forward.py
@@ -14,10 +14,17 @@ def feed_forward_manually(model, x):
     w9, w10 = model.layers[1].weights[1]
     w11, w12 = model.layers[1].weights[2]
     h4 = n.logit_to_prob(h1*w7 + h2*w9 + h3*w11 + 1)
-    h5 = n.logit_to_prob(h1*w8 + h1*w10 + h3*w12 + 1)
+    h5 = n.logit_to_prob(h1*w8 + h2*w10 + h3*w12 + 1)
     
     w13 = model.layers[2].weights[0][0]
     w14 = model.layers[2].weights[1][0]
     
-    y_prob = n.logit_to_prob(h4*w13 + h5*w14 + 1)
-    return y_prob
+    y_prob = n.logit_to_prob(h4*w13 + h5*w14)
+    return {
+        "h1": h1,
+        "h2": h2,
+        "h3": h3,
+        "h4": h4,
+        "h5": h5,
+        "y_prob": y_prob,
+            }
